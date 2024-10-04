a2d6210751a32fe98ced536dce167030e52c0c29
Saint. Clever <parchment@gmx.com>
2024-10-04 13:31:23 -0400

    Add Part. 2 for functions

--- JS_Intro/ALAB 308.5.1: Creating Reusable Functions/index.js
+++ JS_Intro/ALAB 308.5.1: Creating Reusable Functions/index.js
@@ -1,4 +1,4 @@
-// Part 1: Thinking Functionally
+// --- Part 1: Thinking Functionally --- //
 // Take an array of numbers and return the sum.
 let sum = (...nums) => {
   let total = 0;
@@ -37,6 +37,7 @@ let longestString = (arr) => {
   for (let i in arr) {
     if (arr[i].length > letterCount) {
       letterCount = arr[i].length;
+      // NOTE: What if same length?
       // longestWord.shift();
       longestWord.push(arr[i]);
     }
@@ -46,10 +47,52 @@ let longestString = (arr) => {
 }
 
 console.log(longestString(["Take", "an", "array", "of", "strings", "and", "return", "the", "longest", "string.", "strings"]));
+console.log();
 
 
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 // For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+let stringsLongerThan = (arr, num) => {
+  let longerThan = [];
+
+  for (let i in arr) {
+    if (arr[i].length > num) {
+      longerThan.push(arr[i]);
+    }
+  }
+
+  return longerThan;
+}
+
+console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3));
+console.log();
+
 
 // Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+let output = [];
 
+let betweenOneN = (n) => {
+  if (n !== 2) {
+    n--;
+    betweenOneN(n);
+    output.push(n);
+  }
+  
+  return output;
+}
+
+console.log(betweenOneN(20));
+console.log();
+
+
+//--- Part 2: Thinking Methodically ---//
+let arr = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+  { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+  { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+
+
+for (let i in arr) {
+  console.log(arr[i]);
+}
