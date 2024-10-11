00c99902f6aac5393dc56ce8958448003a115c66
Saint. Clever <parchment@gmx.com>
2024-10-11 14:55:13 -0400

    Create addEventListener for textarea button

--- DOM_Intro/ALAB 316.2.1: Interacting with the Browser/app.js
+++ DOM_Intro/ALAB 316.2.1: Interacting with the Browser/app.js
@@ -1,10 +1,21 @@
-// // let question = prompt("What's your name: ");
-// let player1 = document.querySelector("#player1");
 
-// if (question === "nesta") {
-//   alert("correct");
-//   player1.style.width = "1px";
-// } else {
-//   alert("incorrect");
-//   player2,width = "1px";
-// }
+// let question = prompt("What's your name: ");
+let player1 = document.querySelector("#player1");
+let player2 = document.querySelector("#player2");
+let textarea = document.querySelector("textarea");
+let button = document.querySelector("button");
+let question = "javascript";
+
+
+button.addEventListener("click", () => {
+  console.log(textarea.value);
+  textarea.value = '';
+});
+
+(() => {
+  // if (textarea.value === "javascript") {
+  //   player1.style.width = "1px";
+  // } else {
+  //   player2.style.width = "1px";
+  // }
+})();
