fd1fa34f9ad88c1e2263c3c332e5bb3764dfba31
Saint. Clever <parchment@gmx.com>
2024-10-11 14:43:21 -0400

    Update input to textarea, update body, player code styling

---
+++ DOM_Intro/ALAB 316.2.1: Interacting with the Browser/app.js
@@ -0,0 +1,10 @@
+// // let question = prompt("What's your name: ");
+// let player1 = document.querySelector("#player1");
+
+// if (question === "nesta") {
+//   alert("correct");
+//   player1.style.width = "1px";
+// } else {
+//   alert("incorrect");
+//   player2,width = "1px";
+// }
